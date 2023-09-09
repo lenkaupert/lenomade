10ac6f22824430f62fa11af2d7bde5d2115c83c4
Len Kaupert <lenkaupert@Lens-MacBook-Air.local>
2023-09-09 13:48:07 +0200

    add php to fetch image files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>LE NOMADE</title>
+</head>
+<body>
+    <header>
+        <h1>LE NOMADE</h1>
+        <pre>
+🍋
+        </pre>
+    </header>
+    <main>
+        <section class="landscape-images">
+            <?php
+            $imageDirectory = 'landscape_images'; // Directory where images are stored
+            $images = glob($imageDirectory . '/*.{jpg,jpeg,png,gif}', GLOB_BRACE); // Retrieve image files
+
+            foreach ($images as $image) {
+                echo '<img src="' . $image . '" alt="Landscape Image">';
+            }
+            ?>        </section>
+        <section class="portrait-images">
+            <?php
+            $imageDirectory = 'portrait_images'; // Directory where images are stored
+            $images = glob($imageDirectory . '/*.{jpg,jpeg,png,gif}', GLOB_BRACE); // Retrieve image files
+
+            foreach ($images as $image) {
+                echo '<img src="' . $image . '" alt="Portrait Image">';
+            }
+            ?>          </section>
+    </main>
+</body>
+</html>
